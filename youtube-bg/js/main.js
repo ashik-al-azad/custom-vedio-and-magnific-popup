54ad469c51071e95f7592bc9780f3bab984200b2
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-22 14:14:53 +0600

    Add files via upload

---
+++ youtube-bg/js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+    "use strict";
+
+    // youtub player
+    $(".P1").YTPlayer({
+        showControls: false,
+        mute: true,
+        autoPlay: true,
+        loop: true,
+        ratio: "16/9",
+    });
+
+})(jQuery);
