fd64ffca24d9a8e7990f7720a56d171820a19056
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-22 14:11:03 +0600

    Add files via upload

---
+++ Class-31-parallax scrolling/main.js
@@ -0,0 +1,8 @@
+
+$('.parallax-window').parallax({
+  speed: 0.15,
+});
+
+$(document).ready(function(){
+  AOS.init();
+});
---
+++ Class-31-skrollr-master/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	var s = skrollr.init({
+		edgeStrategy: 'set',
+		easing: {
+			WTF: Math.random,
+			inverted: function(p) {
+				return 1-p;
+			}
+		}
+	});
+    
+  });
+
+   
+
+   
